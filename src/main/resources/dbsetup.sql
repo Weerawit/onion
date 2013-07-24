85959cd45f6f14f080979c441e7f57a3531e0819
Weerawit Maneepongsawat <weerawit.m@worldbestsoft.com>
2013-07-24 12:06:20 +0700

    update table & add document_no

--- src/main/resources/dbsetup.sql
+++ src/main/resources/dbsetup.sql
@@ -6,6 +6,21 @@ CREATE SCHEMA IF NOT EXISTS `onion` DEFAULT CHARACTER SET utf8 ;
 USE `onion` ;
 
 -- -----------------------------------------------------
+-- Table `onion`.`document_no`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `onion`.`document_no` ;
+
+CREATE  TABLE IF NOT EXISTS `onion`.`document_no` (
+  `id` BIGINT NOT NULL ,
+  `type` VARCHAR(50) NOT NULL ,
+  `current_val` BIGINT NULL DEFAULT 1 ,
+  `update_date` DATETIME NULL ,
+  PRIMARY KEY (`id`) ,
+  UNIQUE INDEX `type_UNIQUE` (`type` ASC) )
+ENGINE = InnoDB
+
+
+-- -----------------------------------------------------
 -- Table `onion`.`app_user`
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `onion`.`app_user` ;
@@ -80,10 +95,10 @@ CREATE  TABLE IF NOT EXISTS `onion`.`inv_item_group` (
   `id` BIGINT NOT NULL AUTO_INCREMENT COMMENT 'ชนิดของสินค้า\n' ,
   `code` VARCHAR(10) NOT NULL ,
   `name` VARCHAR(50) NOT NULL ,
-  `create_date` DATETIME NULL ,
-  `create_user` VARCHAR(50) NULL ,
-  `update_date` DATETIME NULL ,
-  `update_user` VARCHAR(50) NULL ,
+  `create_date` DATETIME NULL DEFAULT NULL ,
+  `create_user` VARCHAR(50) NULL DEFAULT NULL ,
+  `update_date` DATETIME NULL DEFAULT NULL ,
+  `update_user` VARCHAR(50) NULL DEFAULT NULL ,
   PRIMARY KEY (`id`) ,
   UNIQUE INDEX `code_UNIQUE` (`code` ASC) )
 ENGINE = InnoDB;
@@ -97,13 +112,13 @@ DROP TABLE IF EXISTS `onion`.`inv_item` ;
 CREATE  TABLE IF NOT EXISTS `onion`.`inv_item` (
   `id` BIGINT NOT NULL AUTO_INCREMENT COMMENT 'สินค้า' ,
   `code` VARCHAR(10) NOT NULL ,
-  `name` VARCHAR(50) NULL ,
-  `description` VARCHAR(255) NULL ,
+  `name` VARCHAR(50) NULL DEFAULT NULL ,
+  `description` VARCHAR(255) NULL DEFAULT NULL ,
   `inv_item_group_id` BIGINT NOT NULL ,
-  `create_date` DATETIME NULL ,
-  `create_user` VARCHAR(50) NULL ,
-  `update_date` DATETIME NULL ,
-  `update_user` VARCHAR(50) NULL ,
+  `create_date` DATETIME NULL DEFAULT NULL ,
+  `create_user` VARCHAR(50) NULL DEFAULT NULL ,
+  `update_date` DATETIME NULL DEFAULT NULL ,
+  `update_user` VARCHAR(50) NULL DEFAULT NULL ,
   PRIMARY KEY (`id`) ,
   CONSTRAINT `fk_inv_item_inv_item_group1`
     FOREIGN KEY (`inv_item_group_id` )
@@ -122,13 +137,13 @@ CREATE  TABLE IF NOT EXISTS `onion`.`supplier` (
   `id` BIGINT NOT NULL AUTO_INCREMENT ,
   `code` VARCHAR(10) NOT NULL ,
   `name` VARCHAR(50) NOT NULL ,
-  `address` VARCHAR(255) NULL ,
-  `contact_name` VARCHAR(50) NULL ,
-  `contact_tel` VARCHAR(50) NULL ,
-  `create_date` DATETIME NULL ,
-  `create_user` VARCHAR(50) NULL ,
-  `update_date` DATETIME NULL ,
-  `update_user` VARCHAR(50) NULL ,
+  `address` VARCHAR(255) NULL DEFAULT NULL ,
+  `contact_name` VARCHAR(50) NULL DEFAULT NULL ,
+  `contact_tel` VARCHAR(50) NULL DEFAULT NULL ,
+  `create_date` DATETIME NULL DEFAULT NULL ,
+  `create_user` VARCHAR(50) NULL DEFAULT NULL ,
+  `update_date` DATETIME NULL DEFAULT NULL ,
+  `update_user` VARCHAR(50) NULL DEFAULT NULL ,
   PRIMARY KEY (`id`) ,
   UNIQUE INDEX `code_UNIQUE` (`code` ASC) )
 ENGINE = InnoDB;
@@ -142,14 +157,14 @@ DROP TABLE IF EXISTS `onion`.`inv_goods_receipt` ;
 CREATE  TABLE IF NOT EXISTS `onion`.`inv_goods_receipt` (
   `id` BIGINT NOT NULL AUTO_INCREMENT COMMENT 'ใบรับของสินค้า\n' ,
   `running_no` VARCHAR(20) NOT NULL ,
-  `receipt_date` DATETIME NULL ,
-  `total_cost` DECIMAL(10,2) NULL COMMENT 'ราคารวม ทั้งหมด\n' ,
-  `memo` VARCHAR(255) NULL ,
+  `receipt_date` DATETIME NULL DEFAULT NULL ,
+  `total_cost` DECIMAL(10,2) NULL DEFAULT NULL COMMENT 'ราคารวม ทั้งหมด\n' ,
+  `memo` VARCHAR(255) NULL DEFAULT NULL ,
   `supplier_id` BIGINT NOT NULL COMMENT 'ชื่อบ.ที่ขาย สินค้าให้เรา' ,
-  `create_date` DATETIME NULL ,
-  `create_user` VARCHAR(50) NULL ,
-  `update_date` DATETIME NULL ,
-  `update_user` VARCHAR(50) NULL ,
+  `create_date` DATETIME NULL DEFAULT NULL ,
+  `create_user` VARCHAR(50) NULL DEFAULT NULL ,
+  `update_date` DATETIME NULL DEFAULT NULL ,
+  `update_user` VARCHAR(50) NULL DEFAULT NULL ,
   UNIQUE INDEX `running_no_UNIQUE` (`running_no` ASC) ,
   PRIMARY KEY (`id`) ,
   CONSTRAINT `fk_inv_good_receipt_supplier1`
@@ -167,10 +182,10 @@ DROP TABLE IF EXISTS `onion`.`inv_goods_receipt_item` ;
 
 CREATE  TABLE IF NOT EXISTS `onion`.`inv_goods_receipt_item` (
   `id` BIGINT NOT NULL AUTO_INCREMENT COMMENT 'รายการสินค้า ในใบรับของ\n' ,
-  `qty` DECIMAL(10,2) NULL ,
-  `unit_price` DECIMAL(10,2) NULL ,
-  `unit_type` DECIMAL(10,2) NULL ,
-  `memo` VARCHAR(255) NULL ,
+  `qty` DECIMAL(10,2) NULL DEFAULT NULL ,
+  `unit_price` DECIMAL(10,2) NULL DEFAULT NULL ,
+  `unit_type` DECIMAL(10,2) NULL DEFAULT NULL ,
+  `memo` VARCHAR(255) NULL DEFAULT NULL ,
   `inv_goods_receipt_id` BIGINT NOT NULL ,
   `inv_item_id` BIGINT NOT NULL ,
   PRIMARY KEY (`id`) ,
@@ -194,14 +209,14 @@ DROP TABLE IF EXISTS `onion`.`inv_goods_movement` ;
 
 CREATE  TABLE IF NOT EXISTS `onion`.`inv_goods_movement` (
   `id` BIGINT NOT NULL AUTO_INCREMENT ,
-  `running_no` VARCHAR(20) NULL ,
-  `movement_date` DATETIME NULL ,
-  `movement_type` VARCHAR(3) NULL COMMENT '100 เบิกของ' ,
-  `owner` VARCHAR(50) NULL COMMENT 'ผู้เบิก' ,
-  `create_date` DATETIME NULL ,
-  `create_user` VARCHAR(50) NULL ,
-  `update_date` DATETIME NULL ,
-  `update_user` VARCHAR(50) NULL ,
+  `running_no` VARCHAR(20) NULL DEFAULT NULL ,
+  `movement_date` DATETIME NULL DEFAULT NULL ,
+  `movement_type` VARCHAR(3) NULL DEFAULT NULL COMMENT '100 เบิกของ' ,
+  `owner` VARCHAR(50) NULL DEFAULT NULL COMMENT 'ผู้เบิก' ,
+  `create_date` DATETIME NULL DEFAULT NULL ,
+  `create_user` VARCHAR(50) NULL DEFAULT NULL ,
+  `update_date` DATETIME NULL DEFAULT NULL ,
+  `update_user` VARCHAR(50) NULL DEFAULT NULL ,
   PRIMARY KEY (`id`) )
 ENGINE = InnoDB;
 
@@ -214,8 +229,8 @@ DROP TABLE IF EXISTS `onion`.`inv_goods_movement_item` ;
 CREATE  TABLE IF NOT EXISTS `onion`.`inv_goods_movement_item` (
   `id` BIGINT NOT NULL AUTO_INCREMENT COMMENT 'การเบิกสินค้า ออกจาก stock\n' ,
   `inv_item_id` BIGINT NOT NULL ,
-  `qty` DECIMAL(10,2) NULL ,
-  `memo` VARCHAR(50) NULL COMMENT 'เอกสารไว้อ้างอิง' ,
+  `qty` DECIMAL(10,2) NULL DEFAULT NULL ,
+  `memo` VARCHAR(50) NULL DEFAULT NULL COMMENT 'เอกสารไว้อ้างอิง' ,
   `inv_goods_movement_id` BIGINT NOT NULL ,
   PRIMARY KEY (`id`) ,
   CONSTRAINT `fk_inv_good_movement_inv_item1`
@@ -238,14 +253,14 @@ DROP TABLE IF EXISTS `onion`.`employee` ;
 
 CREATE  TABLE IF NOT EXISTS `onion`.`employee` (
   `id` BIGINT NOT NULL AUTO_INCREMENT ,
-  `first_name` VARCHAR(50) NULL ,
-  `last_name` VARCHAR(50) NULL ,
-  `nick_name` VARCHAR(50) NULL ,
-  `age` INT NULL ,
-  `id_card_no` VARCHAR(50) NULL ,
-  `address` VARCHAR(255) NULL ,
-  `wage` DECIMAL(10,2) NULL ,
-  `memo` VARCHAR(255) NULL ,
+  `first_name` VARCHAR(50) NULL DEFAULT NULL ,
+  `last_name` VARCHAR(50) NULL DEFAULT NULL ,
+  `nick_name` VARCHAR(50) NULL DEFAULT NULL ,
+  `age` INT NULL DEFAULT NULL ,
+  `id_card_no` VARCHAR(50) NULL DEFAULT NULL ,
+  `address` VARCHAR(255) NULL DEFAULT NULL ,
+  `wage` DECIMAL(10,2) NULL DEFAULT NULL ,
+  `memo` VARCHAR(255) NULL DEFAULT NULL ,
   PRIMARY KEY (`id`) )
 ENGINE = InnoDB;
 
@@ -259,11 +274,11 @@ CREATE  TABLE IF NOT EXISTS `onion`.`catalog_type` (
   `id` BIGINT NOT NULL AUTO_INCREMENT ,
   `code` VARCHAR(10) NOT NULL ,
   `name` VARCHAR(50) NOT NULL ,
-  `description` VARCHAR(255) NULL ,
-  `create_date` DATETIME NULL ,
-  `create_user` VARCHAR(50) NULL ,
-  `update_date` DATETIME NULL ,
-  `update_user` VARCHAR(50) NULL ,
+  `description` VARCHAR(255) NULL DEFAULT NULL ,
+  `create_date` DATETIME NULL DEFAULT NULL ,
+  `create_user` VARCHAR(50) NULL DEFAULT NULL ,
+  `update_date` DATETIME NULL DEFAULT NULL ,
+  `update_user` VARCHAR(50) NULL DEFAULT NULL ,
   PRIMARY KEY (`id`) )
 ENGINE = InnoDB;
 
@@ -276,16 +291,16 @@ DROP TABLE IF EXISTS `onion`.`catalog` ;
 CREATE  TABLE IF NOT EXISTS `onion`.`catalog` (
   `id` BIGINT NOT NULL AUTO_INCREMENT ,
   `catalog_type_id` BIGINT NOT NULL ,
-  `code` VARCHAR(10) NULL ,
-  `name` VARCHAR(50) NULL ,
-  `final_price` DECIMAL(10,2) NULL COMMENT 'ราคาขายจริง' ,
-  `est_price` DECIMAL(10,2) NULL COMMENT 'ราคาประเมิณต้นทุน' ,
-  `img` LONGBLOB NULL COMMENT 'images' ,
+  `code` VARCHAR(10) NULL DEFAULT NULL ,
+  `name` VARCHAR(50) NULL DEFAULT NULL ,
+  `final_price` DECIMAL(10,2) NULL DEFAULT NULL COMMENT 'ราคาขายจริง' ,
+  `est_price` DECIMAL(10,2) NULL DEFAULT NULL COMMENT 'ราคาประเมิณต้นทุน' ,
+  `img` LONGBLOB NULL DEFAULT NULL COMMENT 'images' ,
   `inv_item_id` BIGINT NOT NULL ,
-  `update_date` DATETIME NULL ,
-  `create_user` VARCHAR(50) NULL ,
-  `create_date` DATETIME NULL ,
-  `update_user` VARCHAR(50) NULL ,
+  `update_date` DATETIME NULL DEFAULT NULL ,
+  `create_user` VARCHAR(50) NULL DEFAULT NULL ,
+  `create_date` DATETIME NULL DEFAULT NULL ,
+  `update_user` VARCHAR(50) NULL DEFAULT NULL ,
   PRIMARY KEY (`id`) ,
   CONSTRAINT `fk_catalog_catalog_type1`
     FOREIGN KEY (`catalog_type_id` )
@@ -309,12 +324,12 @@ CREATE  TABLE IF NOT EXISTS `onion`.`catalog_item` (
   `id` BIGINT NOT NULL AUTO_INCREMENT ,
   `catalog_id` BIGINT NOT NULL ,
   `inv_item_id` BIGINT NOT NULL COMMENT 'ถ้ามี สินค้าใน ระบบก็ link ให้ด้วย \nเพื่อจะได้ บอกได้ว่า \nวัตถุดิบนั้น มีในระบบหรือเปล่า' ,
-  `name` VARCHAR(50) NULL ,
-  `qty` DECIMAL(10,2) NULL ,
-  `create_date` DATETIME NULL ,
-  `create_user` VARCHAR(50) NULL ,
-  `update_date` DATETIME NULL ,
-  `update_user` VARCHAR(50) NULL ,
+  `name` VARCHAR(50) NULL DEFAULT NULL ,
+  `qty` DECIMAL(10,2) NULL DEFAULT NULL ,
+  `create_date` DATETIME NULL DEFAULT NULL ,
+  `create_user` VARCHAR(50) NULL DEFAULT NULL ,
+  `update_date` DATETIME NULL DEFAULT NULL ,
+  `update_user` VARCHAR(50) NULL DEFAULT NULL ,
   PRIMARY KEY (`id`) ,
   CONSTRAINT `fk_catalog_item_catalog1`
     FOREIGN KEY (`catalog_id` )
@@ -336,17 +351,17 @@ DROP TABLE IF EXISTS `onion`.`customer` ;
 
 CREATE  TABLE IF NOT EXISTS `onion`.`customer` (
   `id` BIGINT NOT NULL AUTO_INCREMENT COMMENT '	' ,
-  `name` VARCHAR(255) NULL ,
-  `customer_type` VARCHAR(3) NULL COMMENT '1-persernal\n2-company\n' ,
-  `shiping_address` VARCHAR(255) NULL ,
-  `billing_address` VARCHAR(255) NULL ,
-  `contact_name` VARCHAR(50) NULL ,
-  `contact_tel` VARCHAR(50) NULL ,
-  `memo` VARCHAR(255) NULL ,
-  `create_date` DATETIME NULL ,
-  `create_user` VARCHAR(50) NULL ,
-  `update_date` DATETIME NULL ,
-  `update_user` VARCHAR(50) NULL ,
+  `name` VARCHAR(255) NULL DEFAULT NULL ,
+  `customer_type` VARCHAR(3) NULL DEFAULT NULL COMMENT '1-persernal\n2-company\n' ,
+  `shiping_address` VARCHAR(255) NULL DEFAULT NULL ,
+  `billing_address` VARCHAR(255) NULL DEFAULT NULL ,
+  `contact_name` VARCHAR(50) NULL DEFAULT NULL ,
+  `contact_tel` VARCHAR(50) NULL DEFAULT NULL ,
+  `memo` VARCHAR(255) NULL DEFAULT NULL ,
+  `create_date` DATETIME NULL DEFAULT NULL ,
+  `create_user` VARCHAR(50) NULL DEFAULT NULL ,
+  `update_date` DATETIME NULL DEFAULT NULL ,
+  `update_user` VARCHAR(50) NULL DEFAULT NULL ,
   PRIMARY KEY (`id`) )
 ENGINE = InnoDB;
 
@@ -358,16 +373,16 @@ DROP TABLE IF EXISTS `onion`.`sale_order` ;
 
 CREATE  TABLE IF NOT EXISTS `onion`.`sale_order` (
   `id` BIGINT NOT NULL AUTO_INCREMENT ,
-  `sale_order_no` VARCHAR(45) NULL COMMENT 'running no' ,
-  `payment_type` VARCHAR(3) NULL COMMENT '1-credit\n2-cash' ,
-  `delivery_date` DATETIME NULL ,
-  `total_price` DECIMAL(10,2) NULL ,
+  `sale_order_no` VARCHAR(45) NULL DEFAULT NULL COMMENT 'running no' ,
+  `payment_type` VARCHAR(3) NULL DEFAULT NULL COMMENT '1-credit\n2-cash' ,
+  `delivery_date` DATETIME NULL DEFAULT NULL ,
+  `total_price` DECIMAL(10,2) NULL DEFAULT NULL ,
   `customer_id` BIGINT NOT NULL ,
-  `status` VARCHAR(3) NULL ,
-  `create_date` DATETIME NULL ,
-  `create_user` VARCHAR(50) NULL ,
-  `update_date` DATETIME NULL ,
-  `update_user` VARCHAR(50) NULL ,
+  `status` VARCHAR(3) NULL DEFAULT NULL ,
+  `create_date` DATETIME NULL DEFAULT NULL ,
+  `create_user` VARCHAR(50) NULL DEFAULT NULL ,
+  `update_date` DATETIME NULL DEFAULT NULL ,
+  `update_user` VARCHAR(50) NULL DEFAULT NULL ,
   PRIMARY KEY (`id`) ,
   CONSTRAINT `fk_sale_order_customer1`
     FOREIGN KEY (`customer_id` )
@@ -386,13 +401,13 @@ CREATE  TABLE IF NOT EXISTS `onion`.`sale_order_item` (
   `id` BIGINT NOT NULL AUTO_INCREMENT ,
   `sale_order_id` BIGINT NOT NULL ,
   `catalog_id` BIGINT NOT NULL ,
-  `qty` DECIMAL(10,2) NULL ,
-  `price_per_unit` DECIMAL(10,2) NULL ,
-  `price` DECIMAL(10,2) NULL ,
-  `create_date` DATETIME NULL ,
-  `create_user` VARCHAR(50) NULL ,
-  `update_date` DATETIME NULL ,
-  `update_user` VARCHAR(50) NULL ,
+  `qty` DECIMAL(10,2) NULL DEFAULT NULL ,
+  `price_per_unit` DECIMAL(10,2) NULL DEFAULT NULL ,
+  `price` DECIMAL(10,2) NULL DEFAULT NULL ,
+  `create_date` DATETIME NULL DEFAULT NULL ,
+  `create_user` VARCHAR(50) NULL DEFAULT NULL ,
+  `update_date` DATETIME NULL DEFAULT NULL ,
+  `update_user` VARCHAR(50) NULL DEFAULT NULL ,
   PRIMARY KEY (`id`) ,
   CONSTRAINT `fk_sale_order_item_sale_order1`
     FOREIGN KEY (`sale_order_id` )
@@ -414,18 +429,18 @@ DROP TABLE IF EXISTS `onion`.`sale_receipt` ;
 
 CREATE  TABLE IF NOT EXISTS `onion`.`sale_receipt` (
   `id` BIGINT NOT NULL AUTO_INCREMENT ,
-  `receipt_no` VARCHAR(20) NULL COMMENT 'running no' ,
+  `receipt_no` VARCHAR(20) NULL DEFAULT NULL COMMENT 'running no' ,
   `sale_order_id` BIGINT NOT NULL ,
-  `receipt_date` DATETIME NULL ,
-  `receipt_type` VARCHAR(3) NULL COMMENT '1- cash\n2- cheque' ,
-  `receipt_amount` DECIMAL(10,2) NULL ,
-  `cheque_no` VARCHAR(50) NULL COMMENT 'for receipt_type = 2 (cheque)' ,
-  `cheque_bank` VARCHAR(50) NULL COMMENT 'for receipt_type = 2 (cheque)' ,
-  `cheque_date` DATETIME NULL COMMENT 'for receipt_type = 2 (cheque)' ,
-  `create_date` DATETIME NULL ,
-  `create_user` VARCHAR(50) NULL ,
-  `update_date` DATETIME NULL ,
-  `update_user` VARCHAR(50) NULL ,
+  `receipt_date` DATETIME NULL DEFAULT NULL ,
+  `receipt_type` VARCHAR(3) NULL DEFAULT NULL COMMENT '1- cash\n2- cheque' ,
+  `receipt_amount` DECIMAL(10,2) NULL DEFAULT NULL ,
+  `cheque_no` VARCHAR(50) NULL DEFAULT NULL COMMENT 'for receipt_type = 2 (cheque)' ,
+  `cheque_bank` VARCHAR(50) NULL DEFAULT NULL COMMENT 'for receipt_type = 2 (cheque)' ,
+  `cheque_date` DATETIME NULL DEFAULT NULL COMMENT 'for receipt_type = 2 (cheque)' ,
+  `create_date` DATETIME NULL DEFAULT NULL ,
+  `create_user` VARCHAR(50) NULL DEFAULT NULL ,
+  `update_date` DATETIME NULL DEFAULT NULL ,
+  `update_user` VARCHAR(50) NULL DEFAULT NULL ,
   PRIMARY KEY (`id`) ,
   CONSTRAINT `fk_sale_receipt_sale_order1`
     FOREIGN KEY (`sale_order_id` )
@@ -443,10 +458,10 @@ DROP TABLE IF EXISTS `onion`.`job_order` ;
 CREATE  TABLE IF NOT EXISTS `onion`.`job_order` (
   `id` BIGINT NOT NULL AUTO_INCREMENT ,
   `sale_order_item_id` BIGINT NOT NULL ,
-  `status` VARCHAR(3) NULL COMMENT '- มาใหม่\r- มอบหมาย\nและกำลังผลิต\r- ผลิตเสร็จแล้ว\r- ส่งมอบ' ,
-  `start_date` DATETIME NULL ,
-  `end_date` DATETIME NULL ,
-  `actual_end_date` DATETIME NULL ,
+  `status` VARCHAR(3) NULL DEFAULT NULL COMMENT '- มาใหม่\r- มอบหมาย\nและกำลังผลิต\r- ผลิตเสร็จแล้ว\r- ส่งมอบ' ,
+  `start_date` DATETIME NULL DEFAULT NULL ,
+  `end_date` DATETIME NULL DEFAULT NULL ,
+  `actual_end_date` DATETIME NULL DEFAULT NULL ,
   `employee_id` BIGINT NOT NULL ,
   PRIMARY KEY (`id`) ,
   CONSTRAINT `fk_job_order_sale_order_item1`
@@ -470,9 +485,8 @@ DROP TABLE IF EXISTS `onion`.`inv_stock` ;
 CREATE  TABLE IF NOT EXISTS `onion`.`inv_stock` (
   `id` BIGINT NOT NULL AUTO_INCREMENT ,
   `inv_item_id` BIGINT NOT NULL ,
-  `qty` DECIMAL(10,2) NULL ,
+  `qty` DECIMAL(10,2) NULL DEFAULT NULL ,
   PRIMARY KEY (`id`) ,
-  UNIQUE INDEX `inv_item_id_UNIQUE` (`inv_item_id` ASC) ,
   CONSTRAINT `fk_inv_stock_inv_item1`
     FOREIGN KEY (`inv_item_id` )
     REFERENCES `onion`.`inv_item` (`id` )
@@ -489,9 +503,9 @@ DROP TABLE IF EXISTS `onion`.`inv_item_level` ;
 CREATE  TABLE IF NOT EXISTS `onion`.`inv_item_level` (
   `id` BIGINT NOT NULL AUTO_INCREMENT ,
   `transaction_date` DATETIME NOT NULL ,
-  `qty_in_stock` DECIMAL(10,2) NULL COMMENT 'มากกว่า0 เป็นการนำเข้า, น้อยกว่า 0 เป็นการเอาออก' ,
-  `ref_document` VARCHAR(50) NULL COMMENT 'key ที่อ้างอิง จาก 2 table\n inv_good_receipt_item, inv_good_movement_item' ,
-  `ref_type` VARCHAR(3) NULL COMMENT '1=goodReceipt(in)\n2=movement (out)' ,
+  `qty_in_stock` DECIMAL(10,2) NULL DEFAULT NULL COMMENT 'มากกว่า0 เป็นการนำเข้า, น้อยกว่า 0 เป็นการเอาออก' ,
+  `ref_document` VARCHAR(50) NULL DEFAULT NULL COMMENT 'key ที่อ้างอิง จาก 2 table\n inv_good_receipt_item, inv_good_movement_item' ,
+  `ref_type` VARCHAR(3) NULL DEFAULT NULL COMMENT '1=goodReceipt(in)\n2=movement (out)' ,
   `inv_item_id` BIGINT NOT NULL ,
   PRIMARY KEY (`id`) ,
   CONSTRAINT `fk_inv_item_level_inv_item1`
