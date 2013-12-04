75c381fab5c25bc6db3158fae239d3ca329cf42b
Weerawit Maneepongsawat <weerawit.m@worldbestsoft.com>
2013-12-04 16:38:20 +0700

    fix bug remove toString with collection (prevent memory leak) update one 2 one bidicrection invItem

--- src/main/java/com/worldbestsoft/model/CatalogType.java
+++ src/main/java/com/worldbestsoft/model/CatalogType.java
@@ -162,21 +162,6 @@ public class CatalogType implements java.io.Serializable {
 	    builder.append(updateDate);
 	    builder.append(", updateUser=");
 	    builder.append(updateUser);
-	    builder.append(", catalogs=");
-	    builder.append(catalogs != null ? toString(catalogs, maxLen) : null);
-	    builder.append("]");
-	    return builder.toString();
-    }
-
-	private String toString(Collection<?> collection, int maxLen) {
-	    StringBuilder builder = new StringBuilder();
-	    builder.append("[");
-	    int i = 0;
-	    for (Iterator<?> iterator = collection.iterator(); iterator.hasNext() && i < maxLen; i++) {
-		    if (i > 0)
-			    builder.append(", ");
-		    builder.append(iterator.next());
-	    }
 	    builder.append("]");
 	    return builder.toString();
     }
--- src/main/java/com/worldbestsoft/model/InvGoodsMovement.java
+++ src/main/java/com/worldbestsoft/model/InvGoodsMovement.java
@@ -187,21 +187,6 @@ public class InvGoodsMovement implements java.io.Serializable {
 	    builder.append(updateDate);
 	    builder.append(", updateUser=");
 	    builder.append(updateUser);
-	    builder.append(", invGoodsMovementItems=");
-	    builder.append(invGoodsMovementItems != null ? toString(invGoodsMovementItems, maxLen) : null);
-	    builder.append("]");
-	    return builder.toString();
-    }
-
-	private String toString(Collection<?> collection, int maxLen) {
-	    StringBuilder builder = new StringBuilder();
-	    builder.append("[");
-	    int i = 0;
-	    for (Iterator<?> iterator = collection.iterator(); iterator.hasNext() && i < maxLen; i++) {
-		    if (i > 0)
-			    builder.append(", ");
-		    builder.append(iterator.next());
-	    }
 	    builder.append("]");
 	    return builder.toString();
     }
